71d85168ba9b0f1ba4119bab50836d0f54cd31c2
NeptuneRjo <rjo9000@outlook.com>
2022-10-16 18:35:35 -0400

    add category fixtures

---
+++ backend/src/__tests__/fixtures.ts
@@ -0,0 +1,16 @@
+export const fakeCategoryPass = {
+	name: 'Fish',
+	links: {
+		LA: 'https://google.com',
+		TSM: 'https://google.com',
+		SF: 'https://google.com',
+	},
+}
+
+export const fakeCategoryFail = {
+	name: 1,
+	links: {
+		LA: 'https://google.com',
+		TSM: 'https://google.com',
+	},
+}
