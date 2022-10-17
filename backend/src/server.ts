f21ea13b110de464da2bfd84ca6c6c7c7d0c4b58
NeptuneRjo <rjo9000@outlook.com>
2022-10-17 19:39:28 -0400

    add routes to server file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,19 @@ import express from 'express'
 import { connection } from 'mongoose'
 
 import './config/mongoServer'
+import { categoryRoutes } from './routes'
 
 const app = express()
 const port = process.env.PORT || 4000
 
+/* Middleware */
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+/* Routes */
+app.use('/api/categories', categoryRoutes)
+
+/* Server */
 connection.on('connected', () => {
 	app.listen(port, () => {
 		console.log('Connected to database and listening on port', port)
