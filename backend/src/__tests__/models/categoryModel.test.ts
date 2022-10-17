92d447e4d5b31cb1f39c5bbf68bf75f729cbf022
NeptuneRjo <rjo9000@outlook.com>
2022-10-16 23:54:58 -0400

    add tests for category model

--- backend/src/__tests__/models/categoryModel.test.ts
+++ backend/src/__tests__/models/categoryModel.test.ts
@@ -6,6 +6,7 @@ import {
 } from '../config/mongoMemoryServer'
 
 import 'jest'
+import { fakeCategoryFail, fakeCategoryPass } from '../fixtures'
 
 describe('Category Model', () => {
 	beforeAll(async () => {
@@ -18,5 +19,34 @@ describe('Category Model', () => {
 
 	afterEach(async () => {
 		await dropCollections()
+	})
+
+	it('creates a new category', async () => {
+		const newCategory = await Category.create(fakeCategoryPass)
+		const { _id, name, links } = newCategory
+
+		expect(_id).toBeDefined()
+		expect(name).toEqual(fakeCategoryPass.name)
+		expect(links?.LA).toEqual(fakeCategoryPass.links.LA)
+		expect(links?.TSM).toEqual(fakeCategoryPass.links.TSM)
+		expect(links?.SF).toEqual(fakeCategoryPass.links.SF)
+	})
+
+	it('fails to create a new category with no content provided', async () => {
+		try {
+			const newCategory = new Category()
+			await newCategory.validate()
+		} catch (error) {
+			expect(error).not.toBeNull()
+		}
+	})
+
+	it('fails to create a new category with incorrect fields', async () => {
+		try {
+			const newCategory = new Category(fakeCategoryFail)
+			await newCategory.validate()
+		} catch (error) {
+			expect(error).not.toBeNull()
+		}
 	})
 })
