66b8cc73dcc4f5b431f30a081d7b874ce1aec54e
NeptuneRjo <rjo9000@outlook.com>
2022-10-17 19:40:17 -0400

    compile

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -6,8 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = require("mongoose");
 require("./config/mongoServer");
+const routes_1 = require("./routes");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 4000;
+/* Middleware */
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+/* Routes */
+app.use('/api/categories', routes_1.categoryRoutes);
+/* Server */
 mongoose_1.connection.on('connected', () => {
     app.listen(port, () => {
         console.log('Connected to database and listening on port', port);
