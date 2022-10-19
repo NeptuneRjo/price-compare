1292d6f731f1689f12ffcb5ea72614677450e44d
NeptuneRjo <rjo9000@outlook.com>
2022-10-18 21:14:25 -0400

    update routes

--- backend/src/routes/categoryRoutes.ts
+++ backend/src/routes/categoryRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import { get_all_categories, get_category } from '../controllers'
+import { get_category, create_category } from '../controllers'
 
 const router = Router()
 
 // /api/categories
 
 router.route('/category/:id').get(get_category)
-router.route('/').get(get_all_categories)
+router.route('/category/create').post(create_category)
 
 export default router
