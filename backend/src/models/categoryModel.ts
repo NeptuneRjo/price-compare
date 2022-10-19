eb0916d2aef682dbd710e66e0277ccda2afa78eb
NeptuneRjo <rjo9000@outlook.com>
2022-10-19 19:15:10 -0400

    changes category model

--- backend/src/models/categoryModel.ts
+++ backend/src/models/categoryModel.ts
@@ -1,62 +1,10 @@
-import { model, Schema, Document, Model } from 'mongoose'
-import Item from './itemModel'
-
-// Define the item that the model creates
-interface CategoryDocument extends Document {
-	name: string
-	links: {
-		LA: string
-		TSM: string
-		SF: string
-	}
-}
-
-// Define the model itself
-interface CategoryModel extends Model<CategoryDocument> {
-	addNewCat(
-		name: string,
-		LA: string,
-		TSM: string,
-		SF: string
-	): Promise<CategoryDocument>
-}
+import { model, Schema } from 'mongoose'
 
 const categoryModel: Schema = new Schema({
 	name: { type: String, required: true },
-	links: {
-		LA: { type: String, default: '', required: true },
-		TSM: { type: String, default: '', required: true },
-		SF: { type: String, default: '', required: true },
-	},
+	links: { type: Array, required: true },
 })
 
-// Exists as a util function if ever needed
-categoryModel.statics.addNewCat = async function (
-	name: string,
-	LA: string,
-	TSM: string,
-	SF: string
-): Promise<CategoryDocument> {
-	const newCat: CategoryDocument = await this.findOneAndUpdate(
-		{
-			name,
-		},
-		{
-			links: {
-				LA,
-				TSM,
-				SF,
-			},
-		},
-		{ setDefaultsOnInsert: true, upsert: true }
-	)
-
-	return newCat
-}
-
-const Category: CategoryModel = model<CategoryDocument, CategoryModel>(
-	'Categorie',
-	categoryModel
-)
+const Category = model('Category', categoryModel)
 
 export default Category
