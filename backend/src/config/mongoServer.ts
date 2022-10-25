b94c97076015d5b98cff8a5f1e38cf1c666a72a6
NeptuneRjo <rjo9000@outlook.com>
2022-10-25 15:12:45 -0400

    env

--- backend/src/config/mongoServer.ts
+++
@@ -1,6 +0,0 @@
-import { connect, connection } from 'mongoose'
-import cfg from '../config'
-
-connect(cfg.mongo.uri)
-
-connection.on('error', console.error.bind(console, 'Mongo connection error'))
